edbc1519058f22c017c5a096f96e86be93065c7c
Kamichita san <satoushogo1@gmail.com>
2025-03-19 20:58:33 +0900

    Create v6path.py

---
+++ v6path.py
@@ -0,0 +1,10 @@
+import os
+
+BOTTOKEN = os.getenv("DISCORD_TOKEN")
+CLIENT_ID = "1351885689263886458"
+CLIENT_SECRET = "68W85LrSCr55Kchcxr6LitHCjjoW0w1k"
+REDIRECT_URI = ""
+
+usadata_path = "userdata.json"
+serverdata_folder_path = "./server/"
+authurl = ""
